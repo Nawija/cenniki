e348970dd351b27c48da8395c189654e933b91c2
Valkon <96625039+Nawija@users.noreply.github.com>
2025-10-30 02:25:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import SidebarServer from "@/components/nav/SidebarServer";
 import "./globals.css";
-import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 
 export default function RootLayout({
@@ -11,7 +10,7 @@ export default function RootLayout({
     return (
         <html lang="pl">
             <body className="m-0 font-sans text-gray-800 bg-gray-100">
-                <ScrollToTopButton />
+                {/* <ScrollToTopButton /> */}
                 <div className="flex min-h-screen">
                     <SidebarServer />
                     <main className="flex-1 md:ml-[260px] p-6 bg-gray-100 min-h-screen">
